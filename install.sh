85d9da2ddae9446a366bbe78b01a8b52327d0cc1
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2021-07-14 07:15:27 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,11 +9,13 @@ elif [ $1 == "linux" ]; then
   wget -q $sm_url -O sourcemod.tar.gz
   tar -xzf sourcemod.tar.gz
   rm sourcemod.tar.gz
- else
+else
   sm_url=$(wget -q -O - "http://www.sourcemod.net/downloads.php?branch=dev" | grep -oE -m1 "https://[a-z.]+/smdrop/[0-9.]+/sourcemod-(.*)-$1.zip")
   wget -q $sm_url -O sourcemod.zip
   unzip sourcemod.zip
   rm sourcemod.zip
+fi
+
 
 mv addons/sourcemod/scripting ./
 rm -r addons
