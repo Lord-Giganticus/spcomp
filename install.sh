ebfe595af9282d849eaa23a908c974d03b7cfdf7
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2021-07-14 07:13:24 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
-sm_url=$(wget -q -O - "http://www.sourcemod.net/downloads.php?branch=dev" | grep -oE -m1 "https://[a-z.]+/smdrop/[0-9.]+/sourcemod-(.*)-$1.tar.gz")
 
-wget -q $sm_url -O sourcemod.tar.gz
-
-tar -xzf sourcemod.tar.gz
-rm sourcemod.tar.gz
+if [ -z $1 ]; then
+  echo "Required Parameter is empty!"
+  exit 1
+elif [ $1 == "linux" ]; then
+  sm_url=$(wget -q -O - "http://www.sourcemod.net/downloads.php?branch=dev" | grep -oE -m1 "https://[a-z.]+/smdrop/[0-9.]+/sourcemod-(.*)-$1.tar.gz")
+  wget -q $sm_url -O sourcemod.tar.gz
+  tar -xzf sourcemod.tar.gz
+  rm sourcemod.tar.gz
+ else
+  sm_url=$(wget -q -O - "http://www.sourcemod.net/downloads.php?branch=dev" | grep -oE -m1 "https://[a-z.]+/smdrop/[0-9.]+/sourcemod-(.*)-$1.zip")
+  wget -q $sm_url -O sourcemod.zip
+  unzip sourcemod.zip
+  rm sourcemod.zip
 
 mv addons/sourcemod/scripting ./
 rm -r addons
