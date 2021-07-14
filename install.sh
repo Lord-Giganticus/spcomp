ae8a19464cd29e1a1f79dae96045713c9cef9714
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2021-07-14 07:20:45 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 
-if [ -z $1 ]; then
+if [[ -z "$1" ]]; then
   echo "Required Parameter is empty!"
   exit 1
-elif [ $1 == "linux" ]; then
+elif [[ "$1" == "linux" ]]; then
   sm_url=$(wget -q -O - "http://www.sourcemod.net/downloads.php?branch=dev" | grep -oE -m1 "https://[a-z.]+/smdrop/[0-9.]+/sourcemod-(.*)-$1.tar.gz")
   wget -q $sm_url -O sourcemod.tar.gz
   tar -xzf sourcemod.tar.gz
